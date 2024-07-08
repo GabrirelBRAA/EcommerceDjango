ec6ce1877e7b2c1d14998788feb564c6bc5217eb
Gabriel <jgbiel29@gmail.com>
2024-07-08 10:57:50 -0300

    Ecommerce app path is now at root instead of at <address>/ecommerce.

--- emoveis/urls.py
+++ emoveis/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("ecommerce/", include("ecommerce.urls"))
+    path("", include("ecommerce.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
